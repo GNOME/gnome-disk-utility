63f14a87a77a6cfffd218c573c73310d31de8b4a
David Zeuthen <davidz@redhat.com>
2008-03-22 12:36:47 -0400

    put a shadow around the scrolled window for the treeview

--- src/gdu-shell.c
+++ src/gdu-shell.c
@@ -552,6 +552,8 @@ create_window (GduShell *shell)
         treeview_scrolled_window = gtk_scrolled_window_new (NULL, NULL);
         gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (treeview_scrolled_window),
                                         GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+        gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (treeview_scrolled_window),
+                                             GTK_SHADOW_IN);
         shell->priv->treeview = GTK_WIDGET (gdu_tree_new (shell->priv->pool));
         gtk_container_add (GTK_CONTAINER (treeview_scrolled_window), shell->priv->treeview);
 

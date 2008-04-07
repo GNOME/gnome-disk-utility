5dea699598883259d71aa9952d320cbf71985152
David Zeuthen <davidz@redhat.com>
2008-04-07 00:48:34 -0400

    ext3 filesystems can be renamed while mounted

--- src/gdu-util.c
+++ src/gdu-util.c
@@ -328,7 +328,7 @@ gdu_util_get_desc_for_part_type (const char *scheme, const char *type)
 /* TODO: retrieve this list from DeviceKit-disks */
 static GduCreatableFilesystem creatable_fstypes[] = {
         {"vfat", 11, TRUE, FALSE},
-        {"ext3", 16, TRUE, FALSE},
+        {"ext3", 16, TRUE, TRUE},
         {"swap", 0, FALSE, FALSE},
         {"ntfs", 255, FALSE, FALSE},
         {"empty", 0, FALSE, FALSE},

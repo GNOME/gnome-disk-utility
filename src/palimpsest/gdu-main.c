94f27580d508812b533bc85170d48e3c33a8625c
David Zeuthen <davidz@redhat.com>
2009-04-11 17:14:01 -0400

    setup textdomain so translations work

--- src/palimpsest/gdu-main.c
+++ src/palimpsest/gdu-main.c
@@ -142,6 +142,10 @@ main (int argc, char **argv)
                 exit (1);
         }
 
+        bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+        bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+        textdomain (GETTEXT_PACKAGE);
+
         gtk_window_set_default_icon_name ("palimpsest");
 
         unique_app = unique_app_new_with_commands ("org.gnome.Palimpsest",

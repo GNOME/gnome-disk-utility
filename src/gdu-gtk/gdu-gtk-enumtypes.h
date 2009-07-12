10533dcf8d11ffca2da0f2f16484f14be0a530dd
David Zeuthen <davidz@redhat.com>
2009-07-12 15:04:14 -0400

    Forgot to add file

---
+++ src/gdu-gtk/gdu-gtk-enumtypes.h
@@ -0,0 +1,21 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __GDU_GTK_ENUM_TYPES_H__
+#define __GDU_GTK_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/* enumerations from "gdu-gtk-enums.h" */
+GType gdu_curve_flags_get_type (void) G_GNUC_CONST;
+#define GDU_TYPE_CURVE_FLAGS (gdu_curve_flags_get_type ())
+GType gdu_curve_unit_get_type (void) G_GNUC_CONST;
+#define GDU_TYPE_CURVE_UNIT (gdu_curve_unit_get_type ())
+G_END_DECLS
+
+#endif /* __GDU_GTK_ENUM_TYPES_H__ */
+
+/* Generated data ends here */
+

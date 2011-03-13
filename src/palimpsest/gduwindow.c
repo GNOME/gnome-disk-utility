62011bf165c871335aef453ded884a252e6ed49c
David Zeuthen <davidz@redhat.com>
2011-03-13 12:04:40 -0400

    Implement UI parts of "Change Partition Type" dialog
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- src/palimpsest/gduwindow.c
+++ src/palimpsest/gduwindow.c
@@ -1092,11 +1092,112 @@ on_change_label (GduWindow *window)
   g_debug ("TODO: %s", G_STRFUNC);
 }
 
+/* ---------------------------------------------------------------------------------------------------- */
+
+typedef struct
+{
+  GtkWidget *dialog;
+  gchar *orig_type;
+  const gchar **part_types;
+} ChangePartitionTypeData;
+
+static void
+on_change_partition_type_combo_box_changed (GtkComboBox *combo_box,
+                                            gpointer     user_data)
+{
+  ChangePartitionTypeData *data = user_data;
+  gint active;
+  gboolean sensitive;
+
+  sensitive = FALSE;
+  active = gtk_combo_box_get_active (combo_box);
+  if (active > 0)
+    {
+      if (g_strcmp0 (data->part_types[active], data->orig_type) != 0)
+        {
+          sensitive = TRUE;
+        }
+    }
+
+  gtk_dialog_set_response_sensitive (GTK_DIALOG (data->dialog),
+                                     GTK_RESPONSE_OK,
+                                     sensitive);
+}
+
 static void
 on_change_partition_type (GduWindow *window)
 {
-  g_debug ("TODO: %s", G_STRFUNC);
+  gint response;
+  GtkWidget *dialog;
+  GtkWidget *combo_box;
+  GDBusObjectProxy *object_proxy;
+  UDisksBlockDevice *block;
+  const gchar *scheme;
+  const gchar *cur_type;
+  const gchar **part_types;
+  guint n;
+  gint active_index;
+  ChangePartitionTypeData data;
+  const gchar *type_to_set;
+
+  object_proxy = gdu_volume_grid_get_selected_device (GDU_VOLUME_GRID (window->volume_grid));
+  g_assert (object_proxy != NULL);
+  block = UDISKS_PEEK_BLOCK_DEVICE (object_proxy);
+  g_assert (block != NULL);
+
+  dialog = gdu_window_get_widget (window, "change-partition-type-dialog");
+  combo_box = gdu_window_get_widget (window, "change-partition-type-combo-box");
+  gtk_window_set_transient_for (GTK_WINDOW (dialog), GTK_WINDOW (window));
+  //gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_OK);
+
+  scheme = udisks_block_device_get_part_entry_scheme (block);
+  cur_type = udisks_block_device_get_part_entry_type (block);
+  part_types = udisks_util_get_part_types_for_scheme (scheme);
+  active_index = -1;
+  gtk_combo_box_text_remove_all (GTK_COMBO_BOX_TEXT (combo_box));
+  for (n = 0; part_types != NULL && part_types[n] != NULL; n++)
+    {
+      const gchar *type;
+      gchar *type_for_display;
+      type = part_types[n];
+      type_for_display = udisks_util_get_part_type_for_display (scheme, type);
+      if (g_strcmp0 (type, cur_type) == 0)
+        active_index = n;
+      gtk_combo_box_text_append (GTK_COMBO_BOX_TEXT (combo_box), NULL, type_for_display);
+      g_free (type_for_display);
+    }
+
+  g_signal_connect (combo_box,
+                    "changed",
+                    G_CALLBACK (on_change_partition_type_combo_box_changed),
+                    &data);
+  memset (&data, '\0', sizeof (ChangePartitionTypeData));
+  data.dialog = dialog;
+  data.orig_type = g_strdup (cur_type);
+  data.part_types = part_types;
+
+  if (active_index > 0)
+    gtk_combo_box_set_active (GTK_COMBO_BOX (combo_box), active_index);
+
+  gtk_widget_show_all (dialog);
+  response = gtk_dialog_run (GTK_DIALOG (dialog));
+  if (response != GTK_RESPONSE_OK)
+    goto out;
+
+  type_to_set = part_types[gtk_combo_box_get_active (GTK_COMBO_BOX (combo_box))];
+
+  g_debug ("TODO: set partition type to %s", type_to_set);
+
+ out:
+  g_signal_handlers_disconnect_by_func (combo_box,
+                                        G_CALLBACK (on_change_partition_type_combo_box_changed),
+                                        &data);
+  gtk_widget_hide (dialog);
+  g_free (part_types);
+  g_free (data.orig_type);
 }
+
+/* ---------------------------------------------------------------------------------------------------- */
 
 static void
 on_change_partition_label (GduWindow *window)

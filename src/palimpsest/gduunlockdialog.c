a1d23726503212b0f5b1ce17153dea1e11c17235
David Zeuthen <davidz@redhat.com>
2012-04-26 16:17:59 -0400

    Check keyring for LUKS passphrase and put it in passphrase entry if available
    Also show a cluebar if this is the case. See
    
     http://people.freedesktop.org/~david/disks-luks-passphrase-from-keyring.png
    
    for a screenshot and
    
     https://bugzilla.gnome.org/show_bug.cgi?id=674161
    
    for where it was decided that it was useful for GVfs to store the
    passphrase in the keyring (in addition to /etc/crypttab).
    
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- src/palimpsest/gduunlockdialog.c
+++ src/palimpsest/gduunlockdialog.c
@@ -23,12 +23,61 @@
 #include "config.h"
 
 #include <glib/gi18n.h>
+#include <gnome-keyring.h>
 
 #include "gduapplication.h"
 #include "gduwindow.h"
 #include "gduunlockdialog.h"
 #include "gduvolumegrid.h"
 #include "gduutils.h"
+
+/* From GVfs's monitor/udisks2/gvfsudisks2volume.c */
+static GnomeKeyringPasswordSchema luks_passphrase_schema =
+{
+  GNOME_KEYRING_ITEM_GENERIC_SECRET,
+  {
+    {"gvfs-luks-uuid", GNOME_KEYRING_ATTRIBUTE_TYPE_STRING},
+    {NULL, 0}
+  }
+};
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+typedef struct
+{
+  UDisksObject *object;
+  UDisksBlock *block;
+  UDisksEncrypted *encrypted;
+
+  GduWindow *window;
+  GtkBuilder *builder;
+
+  GtkWidget *dialog;
+  GtkWidget *infobar_vbox;
+  GtkWidget *entry;
+  GtkWidget *show_passphrase_check_button;
+
+  gchar *passphrase;
+} DialogData;
+
+static void
+dialog_data_free (DialogData *data)
+{
+  if (data->dialog != NULL)
+    {
+      gtk_widget_hide (data->dialog);
+      gtk_widget_destroy (data->dialog);
+    }
+  if (data->object != NULL)
+    g_object_unref (data->object);
+  if (data->window != NULL)
+    g_object_unref (data->window);
+  if (data->builder != NULL)
+    g_object_unref (data->builder);
+
+  g_free (data->passphrase);
+  g_free (data);
+}
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -37,7 +86,7 @@ unlock_cb (UDisksEncrypted *encrypted,
            GAsyncResult    *res,
            gpointer         user_data)
 {
-  GduWindow *window = GDU_WINDOW (user_data);
+  DialogData *data = user_data;
   GError *error;
 
   error = NULL;
@@ -46,83 +95,122 @@ unlock_cb (UDisksEncrypted *encrypted,
                                             res,
                                             &error))
     {
-      gdu_window_show_error (window,
+      gdu_window_show_error (data->window,
                              _("Error unlocking encrypted device"),
                              error);
       g_error_free (error);
     }
-  g_object_unref (window);
+  dialog_data_free (data);
+}
+
+static void
+do_unlock (DialogData *data)
+{
+  udisks_encrypted_call_unlock (data->encrypted,
+                                data->passphrase,
+                                g_variant_new ("a{sv}", NULL), /* options */
+                                NULL, /* cancellable */
+                                (GAsyncReadyCallback) unlock_cb,
+                                data);
+}
+
+static void
+show_dialog (DialogData *data)
+{
+  gint response;
+
+  gtk_widget_show_all (data->dialog);
+  gtk_widget_grab_focus (data->entry);
+
+  response = gtk_dialog_run (GTK_DIALOG (data->dialog));
+  if (response == GTK_RESPONSE_OK)
+    {
+      gtk_widget_hide (data->dialog);
+      data->passphrase = g_strdup (gtk_entry_get_text (GTK_ENTRY (data->entry)));
+      do_unlock (data);
+    }
+  else
+    {
+      /* otherwise, we are done */
+      dialog_data_free (data);
+    }
+}
+
+static void
+luks_find_passphrase_cb (GnomeKeyringResult result,
+                         const gchar       *string,
+                         gpointer           user_data)
+{
+  DialogData *data = user_data;
+
+  /* Don't fail if a keyring error occured... but if we do find a
+   * passphrase then put it into the entry field and show a
+   * cluebar
+   */
+  if (result == GNOME_KEYRING_RESULT_OK)
+    {
+      GtkWidget *infobar;
+      infobar = gdu_utils_create_info_bar (GTK_MESSAGE_INFO,
+                                           _("The encryption passphrase was retrieved from the keyring"),
+                                           NULL);
+      gtk_box_pack_start (GTK_BOX (data->infobar_vbox), infobar, TRUE, TRUE, 0);
+      gtk_entry_set_text (GTK_ENTRY (data->entry), string);
+    }
+  else
+    {
+      gtk_widget_hide (data->infobar_vbox);
+      gtk_widget_set_no_show_all (data->infobar_vbox, TRUE);
+    }
+  show_dialog (data);
 }
 
 void
 gdu_unlock_dialog_show (GduWindow    *window,
                         UDisksObject *object)
 {
-  gint response;
-  GtkBuilder *builder;
-  GtkWidget *dialog;
-  GtkWidget *entry;
-  GtkWidget *show_passphrase_check_button;
-  UDisksBlock *block;
-  UDisksEncrypted *encrypted;
-  const gchar *passphrase;
-  gboolean has_passphrase;
+  gboolean has_passphrase_in_crypttab = FALSE;
+  DialogData *data;
 
-  dialog = NULL;
-  builder = NULL;
+  data = g_new0 (DialogData, 1);
+  data->object = g_object_ref (object);
+  data->block = udisks_object_peek_block (object);
+  data->encrypted = udisks_object_peek_encrypted (object);
+  data->window = g_object_ref (window);
 
-  /* TODO: look up passphrase from gnome-keyring? */
+  data->dialog = GTK_WIDGET (gdu_application_new_widget (gdu_window_get_application (window),
+                                                         "unlock-device-dialog.ui",
+                                                         "unlock-device-dialog",
+                                                         &data->builder));
+  data->infobar_vbox = GTK_WIDGET (gtk_builder_get_object (data->builder, "infobar-vbox"));
+  data->entry = GTK_WIDGET (gtk_builder_get_object (data->builder, "unlock-device-passphrase-entry"));
+  data->show_passphrase_check_button = GTK_WIDGET (gtk_builder_get_object (data->builder, "unlock-device-show-passphrase-check-button"));
 
-  block = udisks_object_peek_block (object);
-  encrypted = udisks_object_peek_encrypted (object);
+  gtk_window_set_transient_for (GTK_WINDOW (data->dialog), GTK_WINDOW (data->window));
+  gtk_dialog_set_default_response (GTK_DIALOG (data->dialog), GTK_RESPONSE_OK);
 
-  passphrase = "";
-  has_passphrase = FALSE;
-  if (gdu_utils_has_configuration (block, "crypttab", &has_passphrase) && has_passphrase)
-    goto do_call;
+  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (data->show_passphrase_check_button), FALSE);
+  gtk_entry_set_text (GTK_ENTRY (data->entry), "");
 
-  dialog = GTK_WIDGET (gdu_application_new_widget (gdu_window_get_application (window),
-                                                   "unlock-device-dialog.ui",
-                                                   "unlock-device-dialog",
-                                                   &builder));
-  entry = GTK_WIDGET (gtk_builder_get_object (builder, "unlock-device-passphrase-entry"));
-  show_passphrase_check_button = GTK_WIDGET (gtk_builder_get_object (builder, "unlock-device-show-passphrase-check-button"));
-
-  gtk_window_set_transient_for (GTK_WINDOW (dialog), GTK_WINDOW (window));
-  gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_OK);
-
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (show_passphrase_check_button), FALSE);
-  gtk_entry_set_text (GTK_ENTRY (entry), "");
-
-  g_object_bind_property (show_passphrase_check_button,
+  g_object_bind_property (data->show_passphrase_check_button,
                           "active",
-                          entry,
+                          data->entry,
                           "visibility",
                           G_BINDING_SYNC_CREATE);
 
-  gtk_widget_show_all (dialog);
-  gtk_widget_grab_focus (entry);
-
-  response = gtk_dialog_run (GTK_DIALOG (dialog));
-  if (response != GTK_RESPONSE_OK)
-    goto out;
-
-  passphrase = gtk_entry_get_text (GTK_ENTRY (entry));
-
- do_call:
-  udisks_encrypted_call_unlock (encrypted,
-                                passphrase,
-                                g_variant_new ("a{sv}", NULL), /* options */
-                                NULL, /* cancellable */
-                                (GAsyncReadyCallback) unlock_cb,
-                                g_object_ref (window));
-
- out:
-  if (dialog != NULL)
+  if (gdu_utils_has_configuration (data->block, "crypttab", &has_passphrase_in_crypttab) &&
+      has_passphrase_in_crypttab)
     {
-      gtk_widget_hide (dialog);
-      gtk_widget_destroy (dialog);
+      data->passphrase = g_strdup ("");
+      do_unlock (data);
     }
-  if (builder != NULL)
-    g_object_unref (builder);
+  else
+    {
+      /* see if there's a passphrase in the keyring */
+      gnome_keyring_find_password (&luks_passphrase_schema,
+                                   luks_find_passphrase_cb,
+                                   data,
+                                   NULL, /* GDestroyNotify */
+                                   "gvfs-luks-uuid", udisks_block_get_id_uuid (data->block),
+                                   NULL); /* sentinel */
+    }
 }

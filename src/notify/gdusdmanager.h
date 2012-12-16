53555aa391b02e69830f1177e64813db60f0ec6e
Giovanni Campagna <gcampagna@src.gnome.org>
2012-12-16 22:43:21 +0100

    Fix compilation of GduSdPlugin with newer gnome-settings-daemon
    gnome-settings-daemon changed the definition of the registration macro,
    which now assumes a Manager object providing start() and stop() methods,
    and no longer lets the plugin implement any code.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=690310

--- src/notify/gdusdmanager.h
+++ src/notify/gdusdmanager.h
@@ -16,14 +16,14 @@
 
 G_BEGIN_DECLS
 
-#define GDU_TYPE_SD_PLUGIN_PLUGIN  (gdu_sd_plugin_get_type ())
-#define GDU_SD_PLUGIN(o)    (G_TYPE_CHECK_INSTANCE_CAST ((o), GDU_TYPE_SD_PLUGIN_PLUGIN, GduSdPlugin))
-#define GDU_IS_SD_PLUGIN_PLUGIN(o) (G_TYPE_CHECK_INSTANCE_TYPE ((o), GDU_TYPE_SD_PLUGIN_PLUGIN))
+#define GDU_TYPE_SD_MANAGER  (gdu_sd_manager_get_type ())
+#define GDU_SD_MANAGER(o)    (G_TYPE_CHECK_INSTANCE_CAST ((o), GDU_TYPE_SD_MANAGER, GduSdManager))
+#define GDU_IS_SD_MANAGER(o) (G_TYPE_CHECK_INSTANCE_TYPE ((o), GDU_TYPE_SD_MANAGER))
 
-struct GduSdPlugin;
-typedef struct GduSdPlugin GduSdPlugin;
+struct GduSdManager;
+typedef struct GduSdManager GduSdManager;
 
-GType gdu_sd_plugin_get_type (void) G_GNUC_CONST;
+GType gdu_sd_manager_get_type (void) G_GNUC_CONST;
 
 /* All the plugins must implement this function */
 G_MODULE_EXPORT GType register_gnome_settings_plugin (GTypeModule *module);

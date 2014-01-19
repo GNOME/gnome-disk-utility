366f30648021145c1ad3120065629ff3c101683f
David Zeuthen <zeuthen@gmail.com>
2014-01-18 17:22:09 -0800

    libgdu: Fix deprecation warnings
    Signed-off-by: David Zeuthen <zeuthen@gmail.com>

--- src/libgdu/gduutils.c
+++ src/libgdu/gduutils.c
@@ -152,24 +152,24 @@ gdu_utils_create_info_bar (GtkMessageType   message_type,
   switch (message_type)
     {
     case GTK_MESSAGE_QUESTION:
-      stock_id = GTK_STOCK_DIALOG_QUESTION;
+      stock_id = "dialog-question";
       break;
 
     default:                 /* explicit fall-through */
     case GTK_MESSAGE_OTHER:  /* explicit fall-through */
     case GTK_MESSAGE_INFO:
-      stock_id = GTK_STOCK_DIALOG_INFO;
+      stock_id = "dialog-information";
       break;
 
     case GTK_MESSAGE_WARNING:
-      stock_id = GTK_STOCK_DIALOG_WARNING;
+      stock_id = "dialog-warning";
       break;
 
     case GTK_MESSAGE_ERROR:
-      stock_id = GTK_STOCK_DIALOG_ERROR;
+      stock_id = "dialog-error";
       break;
     }
-  image = gtk_image_new_from_stock (stock_id, GTK_ICON_SIZE_BUTTON);
+  image = gtk_image_new_from_icon_name (stock_id, GTK_ICON_SIZE_BUTTON);
   gtk_box_pack_start (GTK_BOX (hbox), image, FALSE, TRUE, 0);
 
   label = gtk_label_new (NULL);
@@ -730,7 +730,7 @@ gdu_utils_show_confirmation (GtkWindow    *parent_window,
       scrolled_window = gtk_scrolled_window_new (NULL, NULL);
       gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
       gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolled_window), GTK_SHADOW_OUT);
-      gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scrolled_window), vbox);
+      gtk_container_add (GTK_CONTAINER (scrolled_window), vbox);
       gtk_scrolled_window_set_min_content_height (GTK_SCROLLED_WINDOW (scrolled_window), 125);
 
       gtk_box_pack_start (GTK_BOX (gtk_message_dialog_get_message_area (GTK_MESSAGE_DIALOG (dialog))),

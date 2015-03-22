6ef671ef0d04c58ee1ca88e87e33704a21ba2d88
Michael Catanzaro <mcatanzaro@gnome.org>
2015-03-22 14:19:17 -0500

    Modernize autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
-
 srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+[ -z "$srcdir" ] && srcdir=.
 
-PKG_NAME="gnome-disk-utility"
-
-(test -f $srcdir/src/Makefile.am) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
+if [ ! -f "$srcdir/configure.ac" ]; then
+ echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
+ exit 1
+fi
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME SVN repository"
-    exit 1
+	echo "You need to install gnome-common from GNOME Git"
+	exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+. gnome-autogen.sh "$@"

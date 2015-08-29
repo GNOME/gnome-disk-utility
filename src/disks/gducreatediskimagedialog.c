0354a21aaa94e5b6b536d24e04dd9dbdfe683d08
Michael Catanzaro <mcatanzaro@gnome.org>
2015-08-29 14:07:42 -0500

    Stop using gtk_dialog_set_alternative_button_order
    It's deprecated. It shouldn't be used here. And it doesn't do anything
    here. :)

--- src/disks/gducreatediskimagedialog.c
+++ src/disks/gducreatediskimagedialog.c
@@ -930,10 +930,6 @@ check_overwrite (DialogData *data)
                                             g_file_info_get_display_name (folder_info));
   gtk_dialog_add_button (GTK_DIALOG (dialog), _("_Cancel"), GTK_RESPONSE_CANCEL);
   gtk_dialog_add_button (GTK_DIALOG (dialog), _("_Replace"), GTK_RESPONSE_ACCEPT);
-  gtk_dialog_set_alternative_button_order (GTK_DIALOG (dialog),
-                                           GTK_RESPONSE_ACCEPT,
-                                           GTK_RESPONSE_CANCEL,
-                                           -1);
   gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
   response = gtk_dialog_run (GTK_DIALOG (dialog));
 

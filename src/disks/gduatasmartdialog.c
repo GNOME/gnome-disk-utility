GNOME/gnome-disk-utility

commit da0fbba262a5a88034793de864429fa2dc8bb38a

Placate -Wswitch-default
I don't like this warning but I can live with it.

Surprised this is the only new one....

src/disks/gduatasmartdialog.c
@@ -1654,6 +1654,8 @@ gdu_ata_smart_dialog_show (GduWindow    *window,
         case 2:
           refresh_do (data);
           break;
+        default:
+          g_assert_not_reached ();
         }
 
       if (response < 0)

src/disks/gdubenchmarkdialog.c
@@ -748,6 +748,9 @@ update_updated_label (DialogData *data)
       gtk_label_set_markup (GTK_LABEL (data->updated_label), s);
       g_free (s);
       break;
+
+    default:
+      g_assert_not_reached ();
     }
   G_UNLOCK (bm_lock);
 }
@@ -1657,6 +1660,9 @@ gdu_benchmark_dialog_show (GduWindow    *window,
         case 1: /* abort benchmark */
           abort_benchmark (data);
           break;
+
+        default:
+          g_assert_not_reached ();
         }
 
       if (response < 0)

src/disks/gduvolumegrid.c
@@ -1579,6 +1579,7 @@ grid_element_set_details (GduVolumeGrid  *grid,
   switch (element->type)
     {
     case GDU_VOLUME_GRID_ELEMENT_TYPE_CONTAINER:
+    default:
       g_assert_not_reached ();
       break;
 

src/disks/gduwindow.c
@@ -1687,6 +1687,9 @@ update_all (GduWindow *window)
     case DETAILS_PAGE_DEVICE:
       update_device_page (window, &show_flags);
       break;
+
+    default:
+      g_assert_not_reached ();
     }
   update_for_show_flags (window, &show_flags);
 }
@@ -2908,6 +2911,9 @@ update_device_page (GduWindow      *window,
             case GDU_VOLUME_GRID_ELEMENT_TYPE_FREE_SPACE:
               update_device_page_for_free_space (window, object, block, size, show_flags);
               break;
+
+            default:
+              g_assert_not_reached ();
             }
         }
     }

commit 988f64e9203fc1ddfe7673da4e165caf5ca4ad87

Fix incorrect assertions that were recently added
Fixes crash when using or closing the benchmark and SMART dialogs. This
was a regression from da0fbba262a5a88034793de864429fa2dc8bb38a.

https://bugzilla.gnome.org/show_bug.cgi?id=755206

src/disks/gduatasmartdialog.c
@@ -1642,6 +1642,10 @@ gdu_ata_smart_dialog_show (GduWindow    *window,
     {
       gint response;
       response = gtk_dialog_run (GTK_DIALOG (data->dialog));
+
+      if (response < 0)
+        break;
+
       /* Keep in sync with .ui file */
       switch (response)
         {
@@ -1657,9 +1661,6 @@ gdu_ata_smart_dialog_show (GduWindow    *window,
         default:
           g_assert_not_reached ();
         }
-
-      if (response < 0)
-        break;
     }
 
   g_source_remove (timeout_id);

src/disks/gdubenchmarkdialog.c
@@ -1650,6 +1650,10 @@ gdu_benchmark_dialog_show (GduWindow    *window,
     {
       gint response;
       response = gtk_dialog_run (GTK_DIALOG (data->dialog));
+
+      if (response < 0)
+        break;
+
       /* Keep in sync with .ui file */
       switch (response)
         {
@@ -1664,9 +1668,6 @@ gdu_benchmark_dialog_show (GduWindow    *window,
         default:
           g_assert_not_reached ();
         }
-
-      if (response < 0)
-        break;
     }
 
   g_source_remove (timeout_id);
c5ee1d71d2f89474a8eb67e6b4d7fd537c7fbc12
Iñigo Martínez <inigomartinez@gmail.com>
2017-11-13 19:18:38 +0100

    build: Use datadir on post install script
    The previous approach used a fixed path for datadir. However,
    its location might change.
    
    This patch uses the provided datadir path, if the user provides one,
    or the default path.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=790303

--- meson_post_install.py
+++ meson_post_install.py
@@ -2,14 +2,15 @@
 
 import os
 import subprocess
-
-install_prefix = os.environ['MESON_INSTALL_PREFIX']
-icondir = os.path.join(install_prefix, 'share', 'icons', 'hicolor')
-schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
+import sys
 
 if not os.environ.get('DESTDIR'):
+  datadir = sys.argv[1]
+
+  icondir = os.path.join(datadir, 'icons', 'hicolor')
   print('Update icon cache...')
   subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
 
-  print('Compiling gsettings schemas...')
+  schemadir = os.path.join(datadir, 'glib-2.0', 'schemas')
+  print('Compile gsettings schemas...')
   subprocess.call(['glib-compile-schemas', schemadir])

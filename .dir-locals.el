e9c61aaaf9153f4709abff4b153621385da53069
Mohammed Sadiq <sadiq@sadiqpk.org>
2023-02-22 16:32:08 +0530

    Add .dir-locals
    Since GNU Emacs 29.1, we also have c-ts-mode for C source
    files.  So let's use .dir-locals to manage file variables
    instead of enforcing c-mode.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((indent-tabs-mode . nil)
+         (fill-column . 100)))
+ (c-mode . ((c-file-style . "gnu")
+            (c-file-offsets
+             (brace-list-intro . +))))
+ (c-ts-mode . ((c-ts-mode-style . gnu)))
+)

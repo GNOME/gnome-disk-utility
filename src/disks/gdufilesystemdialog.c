9978a9d4da827b12ea9029634841dde8857f8ad1
Mohammed Sadiq <sadiq@sadiqpk.org>
2023-08-27 12:18:19 +0530

    filesystem-dialog: Use GduDrive to set label

--- src/disks/gdufilesystemdialog.c
+++ src/disks/gdufilesystemdialog.c
@@ -14,6 +14,7 @@
 #include <glib/gi18n.h>
 
 #include "gduutils.h"
+#include "gdu-block.h"
 #include "gduapplication.h"
 #include "gduwindow.h"
 #include "gdufilesystemdialog.h"
@@ -21,38 +22,28 @@
 
 struct _GduFilesystemDialog
 {
-  GtkDialog         parent_instance;
+  GtkDialog          parent_instance;
 
-  GtkLabel         *warning_label;
-  GtkEntry         *filesystem_label_entry;
+  GtkLabel          *warning_label;
+  GtkEntry          *filesystem_label_entry;
 
-  GduWindow        *window;
-  UDisksObject     *disk_object;
-  UDisksFilesystem *filesystem;
-
-  char             *old_label;
-  gboolean          needs_unmount;
+  GduWindow         *window;
+  GduBlock *drive_block;
 };
 
 G_DEFINE_TYPE (GduFilesystemDialog, gdu_filesystem_dialog, GTK_TYPE_DIALOG)
 
 static void
-change_filesystem_label_cb (UDisksFilesystem  *filesystem,
-                            GAsyncResult      *res,
-                            gpointer           user_data)
+change_filesystem_label_cb (GObject      *object,
+                            GAsyncResult *result,
+                            gpointer      user_data)
 {
-  GduFilesystemDialog *self;
-  g_autoptr(GTask) task = user_data;
+  g_autoptr(GduFilesystemDialog) self = user_data;
   g_autoptr(GError) error = NULL;
 
-  g_assert (G_IS_TASK (task));
-
-  self = g_task_get_source_object (task);
   g_assert (GDU_IS_FILESYSTEM_DIALOG (self));
 
-  if (!udisks_filesystem_call_set_label_finish (filesystem,
-                                                res,
-                                                &error))
+  if (!gdu_block_set_fs_label_finish (self->drive_block, result, &error))
     {
       gdu_utils_show_error (GTK_WINDOW (self->window),
                             _("Error setting label"),
@@ -61,67 +52,24 @@ change_filesystem_label_cb (UDisksFilesystem  *filesystem,
 }
 
 static void
-ensure_unused_cb (UDisksObject *object,
-                  GAsyncResult *res,
-                  gpointer      user_data)
-{
-  GduFilesystemDialog *self;
-  g_autoptr(GTask) task = user_data;
-  const char *label;
-
-  g_assert (G_IS_TASK (task));
-
-  self = g_task_get_source_object (task);
-  label = g_task_get_task_data (task);
-  g_assert (GDU_IS_FILESYSTEM_DIALOG (self));
-
-  if (gdu_window_ensure_unused_finish (self->window, res, NULL))
-    {
-      udisks_filesystem_call_set_label (self->filesystem,
-                                        label,
-                                        g_variant_new ("a{sv}", NULL), /* options */
-                                        NULL, /* cancellable */
-                                        (GAsyncReadyCallback) change_filesystem_label_cb,
-                                        g_steal_pointer (&task));
-    }
-}
-
-static void
 filesystem_dialog_response_cb (GduFilesystemDialog *self,
                                int                  response_id)
 {
-  g_autoptr(GTask) task = NULL;
   const char *label;
 
   if (response_id != GTK_RESPONSE_OK)
-    goto end;
+    {
+      gtk_widget_hide (GTK_WIDGET (self));
+      gtk_widget_destroy (GTK_WIDGET (self));
+      return;
+    }
 
   label = gtk_entry_get_text (self->filesystem_label_entry);
 
-  task = g_task_new (self, NULL, NULL, NULL);
-  g_task_set_task_data (task, g_strdup (label), g_free);
-
-  if (self->needs_unmount)
-    {
-      gdu_window_ensure_unused (self->window,
-                                self->disk_object,
-                                (GAsyncReadyCallback) ensure_unused_cb,
-                                NULL, /* cancellable */
-                                g_steal_pointer (&task));
-    }
-  else
-    {
-      udisks_filesystem_call_set_label (self->filesystem,
-                                        label,
-                                        g_variant_new ("a{sv}", NULL), /* options */
-                                        NULL, /* cancellable */
-                                        (GAsyncReadyCallback) change_filesystem_label_cb,
-                                        g_steal_pointer (&task));
-    }
-
- end:
-  gtk_widget_hide (GTK_WIDGET (self));
-  gtk_widget_destroy (GTK_WIDGET (self));
+  gdu_block_set_fs_label_async (self->drive_block,
+                                          label,
+                                          change_filesystem_label_cb,
+                                          g_object_ref (self));
 }
 
 static void
@@ -139,7 +87,8 @@ filesystem_label_entry_changed_cb (GduFilesystemDialog *self,
                                    GTK_ENTRY_ICON_SECONDARY,
                                    NULL);
 
-  if (g_strcmp0 (gtk_entry_get_text (entry), self->old_label) != 0)
+  if (g_strcmp0 (gtk_entry_get_text (entry),
+                 gdu_block_get_fs_label (self->drive_block)) != 0)
     {
       sensitive = TRUE;
     }
@@ -163,9 +112,7 @@ gdu_filesystem_dialog_finalize (GObject *object)
 {
   GduFilesystemDialog *self = (GduFilesystemDialog *)object;
 
-  g_clear_pointer (&self->old_label, g_free);
-  g_clear_object (&self->disk_object);
-  g_clear_object (&self->filesystem);
+  g_clear_object (&self->drive_block);
 
   G_OBJECT_CLASS (gdu_filesystem_dialog_parent_class)->finalize (object);
 }
@@ -202,40 +149,21 @@ gdu_filesystem_dialog_new (void)
 }
 
 static void
-gdu_filesystem_dialog_set_udisks (GduFilesystemDialog *self,
-                                  UDisksObject        *object)
+gdu_filesystem_dialog_set_drive (GduFilesystemDialog *self,
+                                 GduBlock            *drive_partition)
 {
-  const char *const *mount_points;
-  UDisksBlock *block;
-  const char *fs_type;
+  const char *fs_type, *label;
 
-  self->disk_object = g_object_ref (object);
-  self->filesystem = udisks_object_get_filesystem (object);
-  block = udisks_object_peek_block (object);
-  g_assert (self->filesystem != NULL);
-  g_assert (block != NULL);
+  fs_type = gdu_block_get_fs_type (drive_partition);
+  label = gdu_block_get_fs_label (drive_partition);
 
-  mount_points = udisks_filesystem_get_mount_points (self->filesystem);
-  fs_type = udisks_block_get_id_type (block);
-  self->old_label = udisks_block_dup_id_label (block);
-
-  gtk_entry_set_text (GTK_ENTRY (self->filesystem_label_entry), self->old_label);
+  gtk_entry_set_text (GTK_ENTRY (self->filesystem_label_entry), label);
   gtk_editable_select_region (GTK_EDITABLE (self->filesystem_label_entry), 0, -1);
   gtk_entry_set_max_length (self->filesystem_label_entry,
                             gdu_utils_get_max_label_length (fs_type));
 
-  /* Needs to unmount if there's at least one mount point ... */
-  if (mount_points && *mount_points)
-    self->needs_unmount = TRUE;
-
-  /* ... and if they are not ext2, ext3, nor ext4 */
-  if (self->needs_unmount &&
-      (g_strcmp0 (fs_type, "ext2") == 0 ||
-       g_strcmp0 (fs_type, "ext3") == 0 ||
-       g_strcmp0 (fs_type, "ext4") == 0))
-    self->needs_unmount = FALSE;
-
-  gtk_widget_set_visible (GTK_WIDGET (self->warning_label), self->needs_unmount);
+  gtk_widget_set_visible (GTK_WIDGET (self->warning_label),
+                          gdu_block_needs_unmount (drive_partition));
 }
 
 void
@@ -245,7 +173,8 @@ gdu_filesystem_dialog_show (GduWindow    *window,
   GduFilesystemDialog *self;
 
   self = gdu_filesystem_dialog_new ();
-  gdu_filesystem_dialog_set_udisks (self, object);
+  self->drive_block = gdu_block_new (gdu_window_get_client (window), object, NULL, NULL);
+  gdu_filesystem_dialog_set_drive (self, self->drive_block);
   self->window = window;
 
   gtk_window_set_transient_for (GTK_WINDOW (self), GTK_WINDOW (window));

aad4dba93d17d4c7fef7849658b9b9e50ea34059
Mohammed Sadiq <sadiq@sadiqpk.org>
2024-06-11 11:56:43 +0530

    log: Update the right variables
    Otherwise the debugger will break at wrong locations.

--- src/disks/gdu-log.c
+++ src/disks/gdu-log.c
@@ -437,9 +437,9 @@ gdu_log_init (void)
         g_mem_gc_friendly = TRUE;
 
       if (flags & G_LOG_LEVEL_WARNING)
-        fatal_criticals = TRUE;
-      if (flags & G_LOG_LEVEL_CRITICAL)
         fatal_warnings = TRUE;
+      if (flags & G_LOG_LEVEL_CRITICAL)
+        fatal_criticals = TRUE;
 
       stderr_is_journal = g_log_writer_is_journald (fileno (stderr));
       g_log_set_writer_func (gdu_log_handler, NULL, NULL);

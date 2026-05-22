5a59269a355774adc22cbbe7aa12c4330e2fbaed
Inam Ul Haq <inam123451@gmail.com>
2026-05-22 16:25:04 +0530

    libgdu: Fix issues reported by gobject-linter
    gobject-linter reported:
    
    src/libgdu/libgdu.h:9:1: warning: Use #pragma once instead of include guard '__LIB_GDU_H__' [use_pragma_once]
    src/libgdu/libgdu.h:12:1: warning: Includes are not in the correct order. Expected: config.h (if present), associated header, standard C headers, system headers (<>), project headers ("") (all alphabetically sorted within each group, blank line between groups) [include_order]

--- src/libgdu/libgdu.h
+++ src/libgdu/libgdu.h
@@ -6,12 +6,10 @@
  * Author: David Zeuthen <zeuthen@gmail.com>
  */
 
-#ifndef __LIB_GDU_H__
-#define __LIB_GDU_H__
+#pragma once
 
-#include "libgdutypes.h"
+#include "gduutils.h"
+#include "gduutils.h"
 #include "libgduenums.h"
 #include "libgduenumtypes.h"
-#include "gduutils.h"
-
-#endif /* __LIB_GDU_H__ */
+#include "libgdutypes.h"

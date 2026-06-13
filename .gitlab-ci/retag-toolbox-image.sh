36d7b8a8c4daa996e6644dccfe50cfc0f6f0c56c
Inam Ul Haq <inam123451@gmail.com>
2026-06-13 13:03:44 +0530

    ci: Retag the latest container build as "toolbox"

---
+++ .gitlab-ci/retag-toolbox-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$BRANCH_NAME" == "main" ]; then
+  SOURCE_IMAGE="${CI_REGISTRY_IMAGE}/${FDO_REPO_SUFFIX}:${FDO_DISTRIBUTION_TAG}"
+  DEST_IMAGE="${CI_REGISTRY_IMAGE}/${FDO_REPO_SUFFIX}:toolbox"
+
+  echo "Retagging ${SOURCE_IMAGE} as ${DEST_IMAGE}"
+  podman tag "${SOURCE_IMAGE}" "${DEST_IMAGE}"
+  podman push "${DEST_IMAGE}"
+fi

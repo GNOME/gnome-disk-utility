3a05769bd4c6ca544517582ff8bfc2afea60c1cc
Inam Ul Haq <inam123451@gmail.com>
2026-06-16 14:07:52 +0530

    Add gobject-linter configuration

--- src/disks/gdu-rust.h
+++ src/disks/gdu-rust.h
@@ -2,12 +2,9 @@
 
 #include <adwaita.h>
 
-/* gobject-linter-ignore-next-line: missing_implementation */
 extern void gdu_rs_restore_disk_image_dialog_show (GtkWindow *parent_window, const gchar *object_path,
                                                    const gchar *disk_image_filename);
 
-/* gobject-linter-ignore-next-line: missing_implementation */
 extern gboolean gdu_rs_has_local_jobs (void);
 
-/* gobject-linter-ignore-next-line: missing_implementation */
 extern void gdu_rs_local_jobs_clear (void);
